93eb8a65832b023dc2be097ebdc1e83048d5e6e9
kirt@microsoft.com <siharida@microsoft.com>
2025-03-05 12:03:23 +0530

    e

---
+++ 2501300030006711/2501300030006711/Test1.cs
@@ -0,0 +1,24 @@
+﻿namespace ns_2501300030006711
+{
+    [TestClass]
+    public class cls01_2501300030006711
+    {
+        [TestMethod]
+        public void TC01_2501300030006711()
+        {
+
+        }
+
+        [TestMethod]
+        public void TC02_2501300030006711()
+        {
+
+        }
+
+        [TestMethod]
+        public void TC03_2501300030006711()
+        {
+
+        }
+    }
+}
